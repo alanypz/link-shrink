0093bf8112d5b18223bd7f1995a35cd1cba39d86
alanypz <alanypz@gmail.com>
2017-01-09 09:02:07 -0800

    Updated models, utils, and views.

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+def kirr_redirect_view(request, shortcode=None, *args, **kwargs):
+	return HttpResponse("Hello, {sc}".format(sc=shortcode))
+
+class KirrCBView(View):
+	def get(self, request, shortcode=None, *args, **kwargs):
+		return HttpResponse("Hello again, {sc}.".format(sc=shortcode))
